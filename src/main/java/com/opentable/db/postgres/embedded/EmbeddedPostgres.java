5c6804a9a77d8633bb8e7fb4898dbb2a7ce5f746
Marek Malevič <mmalevic@elance-odesk.com>
2016-04-21 15:15:27 +0200

    * style fix

--- src/main/java/com/opentable/db/postgres/embedded/EmbeddedPostgres.java
+++ src/main/java/com/opentable/db/postgres/embedded/EmbeddedPostgres.java
@@ -33,7 +33,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.sql.DataSource;
-import java.io.*;
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.Closeable;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.UnknownHostException;
 import java.nio.channels.FileLock;
