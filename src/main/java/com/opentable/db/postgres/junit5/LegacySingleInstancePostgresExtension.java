000a97e7ebed1ea19914feff6b6f6bffb20d47e9
Michael Bell <mbell@opentable.com>
2022-01-27 10:28:05 -0800

    ok pmd ;)

--- src/main/java/com/opentable/db/postgres/junit5/LegacySingleInstancePostgresExtension.java
+++ src/main/java/com/opentable/db/postgres/junit5/LegacySingleInstancePostgresExtension.java
@@ -42,8 +42,6 @@ public class LegacySingleInstancePostgresExtension implements AfterTestExecution
     private volatile Connection postgresConnection;
     private final List<Consumer<EmbeddedPostgres.Builder>> builderCustomizers = new CopyOnWriteArrayList<>();
 
-    public LegacySingleInstancePostgresExtension() { }
-
     @Override
     public void beforeTestExecution(ExtensionContext context) throws Exception {
         epg = pg();
